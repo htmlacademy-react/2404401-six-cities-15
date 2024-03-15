8fbd20f2f921a807c4b60e60dddaf7f79d1cdcfc
Oleg <beezlerey55@gmail.com>
2024-03-15 18:40:29 +0300

    Создание главной страницы и отрисовка компонента карточки

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app.tsx';
+import {Setting} from './const.ts';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +10,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App
+      messageCount={Setting.messageCount}
+      placesCount={Setting.placesCount}
+      cardsCount={Setting.cardsCount}
+    />
   </React.StrictMode>
 );
